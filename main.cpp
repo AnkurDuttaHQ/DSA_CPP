d26ef87a58aafd02d31290461ab250621bafcd6c
AnkurDev <ankurxcode@gmail.com>
2026-06-18 09:07:15 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main() {    
+    
+    cout << "Hello.....starting the journey with cpp" << endl;
+    cout << "Never be shy to start from 0 , let your dedication to talk" << endl;
+    return 0;
+}
