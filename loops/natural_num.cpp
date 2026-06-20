b483bcbb66d2db1c61e58795e5eebb266cd8f050
AnkurDev <ankurxcode@gmail.com>
2026-06-20 11:38:21 +0530

    Todays session

---
+++ loops/natural_num.cpp
@@ -0,0 +1,12 @@
+// Print N natural Numbers
+#include <iostream>
+using namespace std;
+int main (){
+    int N;
+    cout << "Enter the number:";
+    cin >> N;
+    for(int i=1;i<=N;i++){
+        cout << i << endl;
+    }
+
+}
