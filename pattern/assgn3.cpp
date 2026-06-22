57d1b3e942036f861ebced5b56c1f88b15963598
AnkurDev <ankurxcode@gmail.com>
2026-06-22 12:00:25 +0530

    solving assignment problems of patterns

---
+++ pattern/assgn3.cpp
@@ -0,0 +1,29 @@
+//alindromic number pyramid.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+
+    for(int i = 1; i <= n; i++) {
+
+        // Spaces
+        for(int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        // Descending numbers
+        for(int j = i; j >= 1; j--) {
+            cout << j;
+        }
+
+        // Ascending numbers
+        for(int j = 2; j <= i; j++) {
+            cout << j;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
