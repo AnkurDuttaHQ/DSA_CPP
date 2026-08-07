6ff1beb9a4090ee6f89b5906af77dcde0a2d01fd
AnkurDev <ankurxcode@gmail.com>
2026-08-07 14:33:47 +0530

    solving java problems

---
+++ java/demo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class demo {
+    public static void main(String[] args){
+        LinkedList ll = new LinkedList<>();
+        Stack s = new Stack<>();
+
+        s
+
+        ll.add(11);
+        ll.add(12);
+        ll.add(20);
+        ll.add("Suchi.......");
+
+        System.out.println(ll);
+    }
+}
