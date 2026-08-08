e13822c2fb6e16e50ac879bb84461f9988167e4b
AnkurDev <ankurxcode@gmail.com>
2026-08-08 16:27:03 +0530

    Modified the code with better version

--- Student/Example.java
+++ Student/Example.java
@@ -1,7 +1,12 @@
 package Student;
 class Example {
-    static int x=10;
+    
     public static void main(String[] args) {
-        System.out.println(x);
+        System.out.println("main starts");
+        FIndSum thread1 = new FIndSum();
+        Prod thread2 = new Prod();
+        thread1.start();
+        thread2.start();
+        System.out.println("main ends");
     }
 }
--- Student/Prod.java
+++ Student/Prod.java
@@ -4,7 +4,7 @@ public class Prod extends Thread {
     public void run(){
         try{
             int prod =1;
-            for(int i=0;i<5;i++){
+            for(int i=1;i<5;i++){
                 prod*=i;
                 Thread.sleep(1000);
             }
