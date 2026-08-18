99abbd63ce7b027779a52f249f938efe2d3608f4
AnkurDev <ankurxcode@gmail.com>
2026-08-18 12:00:35 +0530

    modified the code with better time complexity

--- Recursion/assgn3.cpp
+++ Recursion/assgn3.cpp
@@ -13,7 +13,8 @@ int findSubstring(string str, int i , int j , int n){
     }
 
     int res = findSubstring(str,i+1,j,n-1) +
-              findSubstring(str, i , j-1,n-1);
+              findSubstring(str, i , j-1,n-1)-
+              findSubstring(str , i+1 , j-1 , n-2);
 
     if(str[i] == str[j]){
         res ++ ; // if the first and last char are same the res will be ++;
