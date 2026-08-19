bfa16ceb4a03e2d6093d5ad5b694f15e9b5e0248
AnkurDev <ankurxcode@gmail.com>
2026-08-19 16:52:13 +0530

    solve todays assignment problem of java

---
+++ java/prime.java
@@ -0,0 +1,15 @@
+
+class prime {
+    public static void main(String[] args) {
+        int num = 5;
+    
+        for(int i=2;i<num;i++){
+            if(num % i == 0){
+                System.err.println("The number is not prime");
+                break;
+            } else{
+                System.out.println("The number is prime");
+            }
+        }
+    }
+}
